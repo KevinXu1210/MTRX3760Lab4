5fb0959930310895b104a821b91ec6bc62707b25
KevinXu1210 <126121785+KevinXu1210@users.noreply.github.com>
2023-09-15 11:38:57 +0000

    add comment and fix coding style

--- CircuitBuilder.cpp
+++ CircuitBuilder.cpp
@@ -1,52 +1,136 @@
+// This file contains the implementation of the CircuitBuilder class.
+
+
 #include "CircuitBuilder.h"
 
-std::unique_ptr<Circuit> CircuitBuilder::BuildFromStream(std::istream& input) {
-    auto circuit = std::make_unique<Circuit>();
-    std::string token;
-    while (input >> token) {
-        if (token == "component") {
-            ProcessComponent(input, *circuit);
-        } else if (token == "wire") {
-            ProcessWire(input, *circuit);
-        } else if (token == "connect") {
-            ProcessConnect(input);
-        } else if (token == "end") {
-            break;
-        } else {
-            std::string dummyVar;
-            std::getline(input, dummyVar);
-        }
-    }
-    return circuit;
+
+// The BuildFromStream method takes a file stream as input and
+// returns a unique pointer to a Circuit object.
+std::unique_ptr<Circuit> CircuitBuilder::BuildFromStream(std::istream& input) 
+{
+	
+	// Create a unique pointer to a Circuit object
+	auto circuit = std::make_unique<Circuit>();
+
+	// Read the file stream line by line
+	std::string token;
+
+	while (input >> token) 
+	{
+		
+		if (token == "component") 						// If the token is "component",
+		{
+
+			ProcessComponent(input, *circuit);  // call the ProcessComponent method
+
+		}
+		else if (token == "wire") 						// If the token is "wire",
+		{
+
+			ProcessWire(input, *circuit);				// call the ProcessWire method
+
+		} 
+		else if (token == "connect") 					// If the token is "connect",
+		{
+
+			ProcessConnect(input);							// call the ProcessConnect method
+
+		} 
+		else if (token == "end") 
+		{
+
+				break;
+
+		} 
+		else {																// If the token is not one of the above,
+
+			std::string dummyVar;								// read the rest of the line and ignore it
+
+			std::getline(input, dummyVar);
+
+		}
+
+	}
+
+	return circuit;
+
 }
 
-void CircuitBuilder::ProcessComponent(std::istream& input, Circuit& circuit) {
-    std::string type, name;
-    input >> type >> name;
 
-    auto gate = Gate::Create(type, name);
-    gateLookup[name] = gate.get();
-    circuit.AddGate(std::move(gate));
+// It reads the type and name of the gate and creates a new
+// gate using the Gate::Create method.
+void CircuitBuilder::ProcessComponent(std::istream& input, Circuit& circuit) 
+{
+
+	// Read the type and name of the gate
+	std::string type, name;
+
+	// Create a new gate using the Gate::Create method
+	input >> type >> name;
+
+	// Create a unique pointer to a Gate object
+	auto gate = Gate::Create(type, name);
+
+	// Store the gate in the gateLookup map
+	gateLookup[name] = gate.get();
+
+	// Add the gate to the circuit
+	circuit.AddGate(std::move(gate));
+
 }
 
-void CircuitBuilder::ProcessWire(std::istream& input, Circuit& circuit) {
-    std::string wireName;
-    input >> wireName;
 
-    auto wire = std::make_unique<Wire>(wireName);
-    wireLookup[wireName] = wire.get();
-    circuit.AddWire(std::move(wire));
+// It reads the name of the wire and creates a new wire.
+void CircuitBuilder::ProcessWire(std::istream& input, Circuit& circuit) 
+{
+
+	// Read the name of the wire
+	std::string wireName;
+
+	// Create a new wire
+	input >> wireName;
+
+	// Create a unique pointer to a Wire object
+	auto wire = std::make_unique<Wire>(wireName);
+
+	// Store the wire in the wireLookup map
+	wireLookup[wireName] = wire.get(); 
+
+	// Add the wire to the circuit
+	circuit.AddWire(std::move(wire));
+	
 }
 
-void CircuitBuilder::ProcessConnect(std::istream& input) {
-    std::string fromName, toName;
-    int inputIndex;
-    input >> fromName >> toName >> inputIndex;
 
-    Gate* fromGate = gateLookup[fromName];
-    Wire* toWire = wireLookup[toName];
-    fromGate->OutputWire(toWire);
-    if (inputIndex >= 0) {
-        fromGate->InputWire(toWire, inputIndex);
-    }
+// It reads the name of the gate and wire and the index of the
+// input wire and connects them.
+void CircuitBuilder::ProcessConnect(std::istream& input) 
+{
+	
+	// Read the name of the gate and wire and the index of the
+	// input wire
+	std::string fromName, toName;
+
+	int inputIndex;
+
+	// Connect the gate and wire
+	input >> fromName >> toName >> inputIndex;
+
+	// Get the gate and wire from the gateLookup and wireLookup maps
+	Gate* fromGate = gateLookup[fromName];
+
+	Wire* toWire = wireLookup[toName];
+
+	// Connect the gate and wire
+	fromGate->OutputWire(toWire);
+
+	// Check if the inputIndex is greater than or equal to 0
+	if (inputIndex >= 0) 
+	{
+
+		// Connect the gate and wire
+		fromGate->InputWire(toWire, inputIndex);
+
+	}
+
 }
--- CircuitBuilder.h
+++ CircuitBuilder.h
@@ -1,5 +1,15 @@
+// This file contains the CircuitBuilder class, which is used 
+// to build a circuit from a stream.
+// It takes a file stream as input and returns 
+// a unique pointer to a Circuit object.
+// The CircuitBuilder class has three private member variables
+// that are used to store the gates and wires that are created
+// while building the circuit.
+
+
 #ifndef _CIRCUITBUILDER_H_
 #define _CIRCUITBUILDER_H_
+
 
 #include "Circuit.h"
 #include "Gate.h"
@@ -7,17 +17,35 @@
 #include <istream>
 #include <unordered_map>
 
-class CircuitBuilder {
-public:
-    std::unique_ptr<Circuit> BuildFromStream(std::istream& input);
 
-private:
-    void ProcessComponent(std::istream& input, Circuit& circuit);
-    void ProcessWire(std::istream& input, Circuit& circuit);
-    void ProcessConnect(std::istream& input);
+class CircuitBuilder 
+{
 
-    std::unordered_map<std::string, Gate*> gateLookup;
-    std::unordered_map<std::string, Wire*> wireLookup;
+	public:
+
+		// The BuildFromStream method takes a file stream as input and
+		// returns a unique pointer to a Circuit object.
+		std::unique_ptr<Circuit> BuildFromStream(std::istream& input);
+
+	private:
+
+		// It reads the type and name of the gate and creates a new
+		// gate using the Gate::Create method.
+		void ProcessComponent(std::istream& input, Circuit& circuit);
+
+		// It reads the name of the wire and creates a new wire.
+		void ProcessWire(std::istream& input, Circuit& circuit);
+
+		// It reads the name of the gate and wire and the index of the
+		// input wire and connects them.
+		void ProcessConnect(std::istream& input);
+
+		// The gateLookup and wireLookup member variables are used to store
+		// the gates and wires that are created while building the circuit.
+		std::unordered_map<std::string, Gate*> gateLookup;
+		std::unordered_map<std::string, Wire*> wireLookup;
+
 };
+
 
 #endif // _CIRCUITBUILDER_H_
